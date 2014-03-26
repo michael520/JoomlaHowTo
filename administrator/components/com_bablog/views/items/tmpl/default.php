c35399ad7bc8cb326c111fdac77c7e91a81fb447
michael520 <michael@bm-sms.com.tw>
2014-03-26 23:41:41 +0800

    refactor items view.

--- administrator/components/com_bablog/views/items/tmpl/default.php
+++ administrator/components/com_bablog/views/items/tmpl/default.php
@@ -4,4 +4,35 @@
  */
 ?>
 <h1>view for items</h1>
-<?php var_dump($this->items); ?>
+<table class="table table-bordered">
+	<thead>
+	<tr>
+		<th>
+			CreateTime
+		</th>
+		<th>
+			Title
+		</th>
+		<th>
+			Id
+		</th>
+	</tr>
+	</thead>
+	<tbody>
+	<?php foreach ($this->items as $i => $item): ?>
+		<tr>
+			<td>
+				<?php echo $item->createtime; ?>
+			</td>
+			<td>
+				<a href="<?php echo JRoute::_('index.php?option=com_bablog&task=item.edit&id=' . $item->id); ?>">
+					<?php echo $this->escape($item->title); ?>
+				</a>
+			</td>
+			<td>
+				<?php echo $item->id; ?>
+			</td>
+		</tr>
+	<?php endforeach; ?>
+	</tbody>
+</table>
